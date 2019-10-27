82a946328fb23c18dd3eeba7575138b8108e6b9a
Jared Flores <jared_flores@cs.ucsb.edu>
2019-10-27 00:47:50 -0700

    We Movin'

--- game/js/game.js
+++ game/js/game.js
@@ -5,28 +5,41 @@ var OP_JOIN = 0x00;
 	OP_SMOVE = 0x04;
 	OP_PROG = 0x05;
 	OP_SPEC = 0x06;
-	OP_STOP = 0x07;
+	OP_STOP = 0x07,
+	OP_WAIT = 0x08,
+	MAX_VELOCITY_PLAYER = 2
+	MAX_VELOCITY_BOSS = 1;
 
 function mmcbfbbr() {
 	var img_name = "",
 		knight_imgs = [],
+		spotlight_imgs = [],
+		waffle_img = new Image();
 		loaded = 0,
 		errored = 0,
-		knights_ready = false;
+		knights_ready = false,
+		waffle_ready = false;
 
 	function knight_load() {
 		loaded += 1;
-		if( loaded + errored == 20 ) {
+		if( loaded + errored == 40 ) {
 			knights_ready = knights_ready || true;
 		}
 	}
 
 	function knight_error() {
 		errored += 1;
-		if( loaded + errored == 20 ) {
+		if( loaded + errored == 40 ) {
 			knights_ready = knights_ready || true;
 		}
 	}
+
+	function waffle_load() {
+		waffle_ready = true;
+	}
+
+	waffle_img.addEventListener( "load", waffle_load );
+	waffle_img.src = "assets/waffle.png";
 
 	for( var i = 1; i <= 20; i++ ) {
 		img_name = "knight_";
@@ -37,6 +50,13 @@ function mmcbfbbr() {
 		knight_imgs[ i - 1 ].addEventListener( "load", knight_load );
 		knight_imgs[ i - 1 ].addEventListener( "load", knight_error );
 		knight_imgs[ i - 1 ].src = "assets/" + img_name;
+		img_name = "spotlight_";
+		img_name += i.toString();
+		img_name += ".png";
+		spotlight_imgs.push( new Image() );
+		spotlight_imgs[ i - 1 ].addEventListener( "load", knight_load );
+		spotlight_imgs[ i - 1 ].addEventListener( "load", knight_error );
+		spotlight_imgs[ i - 1 ].src = "assets/" + img_name;
 	}
 
 	function Player( uid, name ) {
@@ -46,6 +66,15 @@ function mmcbfbbr() {
 		this.y = 0;
 		this.direction = 0;
 		this.magnitude = 0;
+		this.dx = 0;
+		this.dy = 0;
+		this.sx = 0;
+		this.sy = 0;
+		this.sdirection = 0;
+		this.smagnitude = 0;
+		this.sdx = 0
+		this.sdy = 0
+		this.spotlight = false;
 	}
 
 	function Game() {
@@ -54,6 +83,8 @@ function mmcbfbbr() {
 		this.players = {};
 		this.countdown = 5;
 		this.interval = null;
+		this.started = false;
+		this.boss = -1;
 	}
 
 	var ws = new WebSocket( "wss://mmcbfbbr.herokuapp.com" ),
@@ -100,6 +131,7 @@ function mmcbfbbr() {
 			startscreen.setAttribute( "class", "hidden" );
 			gamescreen.setAttribute( "class", "" );
 			ws.send( String.fromCharCode( OP_START ) );
+			game.started = true;
 		}
 	}
 
@@ -110,15 +142,104 @@ function mmcbfbbr() {
 		countdown.innerHTML = "5";
 		cdtext.setAttribute( "class", "" );
 		game.interval = setInterval( countdown_game, 1000 );
+		ws.send( String.fromCharCode( OP_WAIT ) );
+	}
+
+	function update( elapsed ) {
+		var players = Object.values( game.players ),
+			MAX_VELOCITY = MAX_VELOCITY_PLAYER,
+			width = 14,
+			height = 20,
+			player;
+		for( var i = 0; i < players.length; i++ ) {
+			player = players[ i ];
+			if( player.uid == game.boss ) {
+				MAX_VELOCITY = MAX_VELOCITY_BOSS;
+				width = 70;
+				height = 94;
+			}
+			if( player.spotlight ) {
+				player.sdy += ( Math.sin( player.sdirection ) * player.smagnitude * 50 ) * .000001 * elapsed;
+				player.sdx += ( Math.cos( player.sdirection ) * player.smagnitude * 50 ) * .000001 * elapsed;
+				if( player.sdy < -MAX_VELOCITY ) {
+					player.sdy = -MAX_VELOCITY;
+				} else if( player.sdy > MAX_VELOCITY ) {
+					player.sdy = MAX_VELOCITY;
+				}
+				if( player.sdx < -MAX_VELOCITY ) {
+					player.sdx = -MAX_VELOCITY;
+				} else if( player.sdx > MAX_VELOCITY ) {
+					player.sdy = MAX_VELOCITY;
+				}
+				player.sy += player.sdy;
+				player.sx += player.sdx;
+			} else {
+				player.dy += ( Math.sin( player.direction ) * player.magnitude * 50 ) * .000001 * elapsed;
+				player.dx += ( Math.cos( player.direction ) * player.magnitude * 50 ) * .000001 * elapsed;
+				if( player.dy < -MAX_VELOCITY ) {
+					player.dy = -MAX_VELOCITY;
+				} else if( player.dy > MAX_VELOCITY ) {
+					player.dy = MAX_VELOCITY;
+				}
+				if( player.dx < -MAX_VELOCITY ) {
+					player.dx = -MAX_VELOCITY;
+				} else if( player.dx > MAX_VELOCITY ) {
+					player.dx = MAX_VELOCITY;
+				}
+			}
+			console.log( "dx: " + player.dx.toString() + " dy: " + player.dy.toString() );
+			player.y += player.dy;
+			player.x += player.dx;
+			if( player.y < 0 ) {
+				player.y = 0;
+				if( player.dy < 0 ) {
+					player.dy = 0;
+				}
+			}
+			if( player.y + height > game.canvas.height ) {
+				player.y = game.canvas.height - height;
+				if( player.dy > 0 ) {
+					player.dy = 0;
+				}
+			}
+			if( player.x < 0 ) {
+				player.x = 0;
+				if( player.dx < 0 ) {
+					player.dx = 0;
+				}
+			}
+			if( player.x + width > game.canvas.width ) {
+				player.x = game.canvas.width - width;
+				if( player.dx > 0 ) {
+					player.dx = 0;
+				}
+			}
+		}
+	}
+
+	function render_players() {
+		var players = Object.values( game.players ),
+			player,
+			waffle;
+		for( var i = 0; i < players.length; i++ ) {
+			player = players[ i ];
+			if( game.boss != player.uid ) {
+				game.ctx.drawImage( knight_imgs[ player.uid - 1 ], 0, 0, 14, 20, player.x | 0, player.y | 0, 14, 20 );
+			}
+		}
+		waffle = game.players[ game.boss ];
+		game.ctx.drawImage( waffle_img, 0, 0, 70, 94, waffle.x | 0, waffle.y | 0, 70, 94 );
 	}
 
 	function game_loop( ts ) {
 		if( game.canvas != null ) {
 			if( !game.lastTS ) game.lastTS = ts;
-			game.lastTS = ts;
-			if( knights_ready ) {
-
+			if( game.started && knights_ready ) {
+				game.ctx.clearRect( 0, 0, game.canvas.width, game.canvas.height );
+				update( ts - game.lastTS );
+				render_players();
 			}
+			game.lastTS = ts;
 		}
 		window.requestAnimationFrame( game_loop );
 	}
@@ -136,6 +257,7 @@ function mmcbfbbr() {
 	function ws_msg( msg ) {
 		var code = msg.data.charCodeAt( 0 ),
 			data = msg.data.substring( 1 );
+		console.log( code );
 		switch( code ) {
 			case OP_JOIN:
 				game.players[ data.charCodeAt( 0 ) ] = new Player( data.charCodeAt( 0 ), data.substring( 1 ) );
@@ -146,8 +268,19 @@ function mmcbfbbr() {
 				player.magnitude = data.charCodeAt( 3 );
 				player.direction = data.charCodeAt( 1 ) << 8;
 				player.direction += data.charCodeAt( 2 );
+				player.direction = player.direction / 180 * Math.PI;
 				console.log( "magnitude: " + player.magnitude.toString() + " direction: " + player.direction.toString() )
+				break;
 			case OP_SMOVE:
+				var player = game.players[ data.charCodeAt( 0 ) ];
+				player.smagnitude = data.charCodeAt( 3 );
+				player.sdirection = data.charCodeAt( 1 ) << 8;
+				player.sdirection += data.charCodeAt( 2 );
+				player.sdirection = player.direction / 180 * Math.PI;
+				console.log( "magnitude: " + player.magnitude.toString() + " direction: " + player.direction.toString() )
+				break;
+			case OP_WAIT:
+				game.boss = data.charCodeAt( 0 );
 				break;
 		}
 	}
