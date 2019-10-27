29d6ef3f0b2ecac839e7241089391c977fac9bb1
Jared Flores <jared_flores@cs.ucsb.edu>
2019-10-26 21:29:19 -0700

    Direction & Magnitude!

--- game/js/game.js
+++ game/js/game.js
@@ -8,9 +8,44 @@ var OP_JOIN = 0x00;
 	OP_STOP = 0x07;
 
 function mmcbfbbr() {
+	var img_name = "",
+		knight_imgs = [],
+		loaded = 0,
+		errored = 0,
+		knights_ready = false;
+
+	function knight_load() {
+		loaded += 1;
+		if( loaded + errored == 20 ) {
+			knights_ready = knights_ready || true;
+		}
+	}
+
+	function knight_error() {
+		errored += 1;
+		if( loaded + errored == 20 ) {
+			knights_ready = knights_ready || true;
+		}
+	}
+
+	for( var i = 1; i <= 20; i++ ) {
+		img_name = "knight_";
+		if( i < 10 ) img_name += "0";
+		img_name += i.toString();
+		img_name += ".png";
+		knight_imgs.push( new Image() );
+		knight_imgs[ i - 1 ].addEventListener( "load", knight_load );
+		knight_imgs[ i - 1 ].addEventListener( "load", knight_error );
+		knight_imgs[ i - 1 ].src = "assets/" + img_name;
+	}
+
 	function Player( uid, name ) {
 		this.uid = uid;
 		this.name = name;
+		this.x = 0;
+		this.y = 0;
+		this.direction = 0;
+		this.magnitude = 0;
 	}
 
 	function Game() {
@@ -40,6 +75,22 @@ function mmcbfbbr() {
 		game.startTS = null;
 	}
 
+	function refresh_players() {
+		var players = Object.values( game.players ),
+			span,
+			col;
+		lists[ 0 ].innerHTML = "";
+		lists[ 1 ].innerHTML = "";
+		lists[ 2 ].innerHTML = "";
+		for( var i = 0; i < players.length; i++ ) {
+			span = document.createElement( "span" );
+			span.setAttribute( "class", "player-name" );
+			span.innerHTML = players[ i ].name;
+			col = lists[ i % 3 ];
+			col.appendChild( span );
+		}
+	}
+
 	function countdown_game() {
 		game.countdown -= 1;
 		if( game.countdown >= 0 ) {
@@ -48,6 +99,7 @@ function mmcbfbbr() {
 			clearInterval( game.interval );
 			startscreen.setAttribute( "class", "hidden" );
 			gamescreen.setAttribute( "class", "" );
+			ws.send( String.fromCharCode( OP_START ) );
 		}
 	}
 
@@ -64,13 +116,16 @@ function mmcbfbbr() {
 		if( game.canvas != null ) {
 			if( !game.lastTS ) game.lastTS = ts;
 			game.lastTS = ts;
+			if( knights_ready ) {
+
+			}
 		}
 		window.requestAnimationFrame( game_loop );
 	}
 
 	function ws_open() {
-		ws.send( String.fromCharCode( 0x07 ) );
-		ws.send( String.fromCharCode( 0x01 ) );
+		ws.send( String.fromCharCode( OP_STOP ) );
+		ws.send( String.fromCharCode( OP_GAME ) );
 		init_game();
 	}
 
@@ -79,12 +134,20 @@ function mmcbfbbr() {
 	}
 
 	function ws_msg( msg ) {
-		code = msg.data.charCodeAt( 0 );
-		data = msg.data.substring( 1 ); 
+		var code = msg.data.charCodeAt( 0 ),
+			data = msg.data.substring( 1 );
 		switch( code ) {
 			case OP_JOIN:
 				game.players[ data.charCodeAt( 0 ) ] = new Player( data.charCodeAt( 0 ), data.substring( 1 ) );
-				console.log( game );
+				refresh_players();
+				break;
+			case OP_PMOVE:
+				var player = game.players[ data.charCodeAt( 0 ) ];
+				player.magnitude = data.charCodeAt( 3 );
+				player.direction = data.charCodeAt( 1 ) << 8;
+				player.direction += data.charCodeAt( 2 );
+				console.log( "magnitude: " + player.magnitude.toString() + " direction: " + player.direction.toString() )
+			case OP_SMOVE:
 				break;
 		}
 	}
